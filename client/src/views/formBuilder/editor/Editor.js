352dec0eeb25fbcf6310453474222ac054942e62
ghostcommander0102 <ghostcommander0102@outlook.com>
2023-12-08 08:24:16 -0500

    Update Sidebar issue

--- client/src/views/formBuilder/editor/Editor.js
+++ client/src/views/formBuilder/editor/Editor.js
@@ -1062,7 +1062,7 @@ export default function Editor({
                       )
                     }
                     {
-                      sidebarData.menu.id != 'quick-add' && sidebarData.menu.id != 'blog' && sidebarData.menu.id !== 'cms' && (
+                      sidebarData.menu.id != 'quick-add' && sidebarData.menu.id != 'blog' && sidebarData.menu.id !== 'cms' && sidebarData.menu.id !== 'store' && (
                       <div className='submenu-and-element d-flex'>
                         <div className="submenu-list">
                           {
