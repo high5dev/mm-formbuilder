72561c019d7f8dc949439f09be36badb57250754
mostakim-h <iamlearner.forme@gmail.com>
2024-01-02 12:06:23 +0600

    UI and Functionality added

--- client/src/views/webBuilder/createDetail/tabs/pages/leftSidebar.js
+++ client/src/views/webBuilder/createDetail/tabs/pages/leftSidebar.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ListGroup, ListGroupItem} from 'reactstrap';
+import {getUserData} from "../../../../../auth/utils";
+import {Trash} from "react-feather";
 
-export const Sidebar = () => {
-    // ** STATE
+export const Sidebar = ({active, setActive, dispatch, store}) => {
+
     return (
         <div className="sidebar" style={{width: '260px', height: 'calc(100vh - 2rem)'}}>
             <div className="sidebar-content task-sidebar">
                 <div className="task-app-menu">
 
                     <div className="create-workspace-btn m-1">
-                       <h4 className={'text-center'}>PAGES</h4>
+                        <h4 className={'text-center'}>PAGES</h4>
                     </div>
 
                     <ListGroup className={'mx-1 mt-2'}>
-                        <ListGroupItem
-                            active
-                            tag="a"
-                        >
-                            Item 1
-                        </ListGroupItem>
-                        <ListGroupItem
-                            tag="a"
-                        >
-                            Item 2
-                        </ListGroupItem>
-                        <ListGroupItem
-                            tag="a"
-                        >
-                            Item 3
-                        </ListGroupItem>
+                        {store?.form &&
+                            store?.form?.formData?.map((x, idx) => {
+                                return (
+                                    <ListGroupItem
+                                        key={idx}
+                                        active={active === x?.id}
+                                        onClick={() => setActive(x?.id)}
+                                        style={{cursor: 'pointer'}}
+                                    >
+
+                                        <div className="ws-name">
+                                            <span> {x?.name}</span>
+                                        </div>
+                                        <div>
+                                            {x?.name ? <Trash size={16}/> : 'No tab available'}
+                                        </div>
+                                    </ListGroupItem>
+                                );
+                            })}
                     </ListGroup>
                 </div>
             </div>
