dd487cc954688f50efd52d707dfc10d1fc594841
champion5star <champion5star247963@gmail.com>
2024-01-24 06:13:52 +0000

    fix mobile friendly

--- client/src/views/webBuilder/editor/elements/image/image.js
+++ client/src/views/webBuilder/editor/elements/image/image.js
@@ -6,7 +6,7 @@ let imageItem = {
         tagName: 'img',
         draggable: true,
         droppable: true,
-        attributes: { class: 'photo-element', src:"https://i.ibb.co/xM56xB3/image-large-3.png"},
+        attributes: { class: 'photo-element', src:"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMDAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3R5bGU9ImZpbGw6IHJnYmEoMCwwLDAsMC4xNSk7IHRyYW5zZm9ybTogc2NhbGUoMC43NSkiPgogICAgICAgIDxwYXRoIGQ9Ik04LjUgMTMuNWwyLjUgMyAzLjUtNC41IDQuNSA2SDVtMTYgMVY1YTIgMiAwIDAgMC0yLTJINWMtMS4xIDAtMiAuOS0yIDJ2MTRjMCAxLjEuOSAyIDIgMmgxNGMxLjEgMCAyLS45IDItMnoiPjwvcGF0aD4KICAgICAgPC9zdmc+"},
         styles: `
         .photo-element{display:block; width:fit-content;width:100px; height:100px}
         `,
